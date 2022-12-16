ce6721e49b2afaf17d835d93419054627a680346
OlegGubanov <84533650+OlegGubanov@users.noreply.github.com>
2022-12-16 12:40:36 +0500

    Добавьте файлы проекта.

---
+++ QR-and-Barcode-Generator/Program.cs
@@ -0,0 +1,52 @@
+﻿using QRCoder;
+using BarcodeLib;
+using System.Drawing;
+
+while (true)
+{
+    Console.WriteLine("QRCode, Barcode");
+    switch (Console.ReadLine())
+    {
+        case "QRCode":
+            {
+                Console.WriteLine("Введите текст для кодирования");
+                var text = Console.ReadLine();
+                EncodeAsQRCode(text);
+                break;
+            }
+        case "Barcode":
+            {
+                Console.WriteLine("Введите текст для кодирования");
+                var text = Console.ReadLine();
+                try
+                {
+                    EncodeAsBarcode(text);
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                    goto case "Barcode";
+                }
+                break;
+            }
+        default:
+            break;
+    }
+
+}
+
+void EncodeAsQRCode(string text)
+{
+    QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+    QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+    QRCode qrCode = new QRCode(qrCodeData);
+    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+    qrCodeImage.Save("QRCode.jpg");
+}
+
+void EncodeAsBarcode(string text)
+{
+    Barcode barcode = new Barcode();
+    barcode.Encode(TYPE.EAN13, text);
+    barcode.SaveImage("Barcode.jpg", SaveTypes.JPG);
+}
